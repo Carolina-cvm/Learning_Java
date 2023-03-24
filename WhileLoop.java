d2862c46a2a4888680de1f063afe667d1c1e60e3
Carolina <55239200+Carolina-cvm@users.noreply.github.com>
2023-03-24 14:08:29 -0300

    Create WhileLoop.java

---
+++ WhileLoop.java
@@ -0,0 +1,18 @@
+public class WhileLoop {
+  public static void main(String[] args) {
+    // WHILE
+    int i = 0;
+    while (i <= 5) {
+      System.out.println(i);
+      i++;
+    } // 1 2 3 4 5
+
+    // DO WHILE
+    int n = 0;
+    do {
+      System.out.println(n);
+      n++;
+    }
+    while (n <= 10); // 1 2 3 4 5 6 7 8 9 10
+  }
+}
