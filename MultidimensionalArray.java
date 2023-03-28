8c363e0337571cd3a803026c340d8534a0e12fd6
Carolina <55239200+Carolina-cvm@users.noreply.github.com>
2023-03-28 14:25:33 -0300

    Create MultidimensionalArray.java

---
+++ MultidimensionalArray.java
@@ -0,0 +1,15 @@
+public class MultidimensionalArray {
+  public static void main(String[] args){
+    // multidimensional array is an array of arrays.
+    int[][] myNumbers = { {1, 2, 3, 4}, {5, 6, 7} };
+    System.out.println(myNumbers[1][2]); // 7
+    myNumbers[1][2] = 9;
+    System.out.println(myNumbers[1][2]); // 9 instead of 7
+    // FOR
+    for (int i = 0; i < myNumbers.length; ++i) {
+      for(int j = 0; j < myNumbers[i].length; ++j) {
+        System.out.println(myNumbers[i][j]); // 1 2 3 4 5 6 7
+      }
+    }
+  }
+}
