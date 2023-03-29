07897a8ddd5be8132e3024e56afa0ec30ca181a1
Carolina <55239200+Carolina-cvm@users.noreply.github.com>
2023-03-29 14:02:35 -0300

    Create Overloading.java

---
+++ Overloading.java
@@ -0,0 +1,16 @@
+public class Overloading {
+  static int conta(int x, int y) {
+    return x + y;
+  }
+  
+  static double conta(double x, double y) {
+    return x + y;
+  }
+  
+  public static void main(String[] args) {
+    int number1 = conta(12, 8);
+    double number2 = conta(2.25, 9.30);
+    System.out.println("Conta int: " + number1);
+    System.out.println("Conta double: " + number2);
+  }
+}
