d3a0fad3f45a047282493c3b998b533b83ae97c1
Carolina <55239200+Carolina-cvm@users.noreply.github.com>
2023-03-30 14:11:35 -0300

    Create ClassesObjects.java

---
+++ ClassesObjects.java
@@ -0,0 +1,11 @@
+public class ClassesObjects {
+  int numX = 50;
+  int numY = 100;
+
+  public static void main(String[] args) {
+    Main object1 = new Main();
+    Main object2 = new Main();
+    System.out.println(object1.numX); // 50
+    System.out.println(object2.numY); // 100
+  }
+}
