fdfa86196ad63a50c3ee5a8c99f1f1655b261f0f
Carolina <55239200+Carolina-cvm@users.noreply.github.com>
2023-04-03 13:55:00 -0300

    Create Encapsulation.java

---
+++ Encapsulation.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(String[] args) {
+    // get and set
+    Person obj = new Person();
+    obj.setName("Maria");
+    System.out.println(obj.getName());
+  }
+}
