bde700908e397e1517c03cab857d813f59aa79b8
Carolina <55239200+Carolina-cvm@users.noreply.github.com>
2023-04-03 13:54:32 -0300

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,15 @@
+public class Person {
+  // get and set
+  //declare class variables/attributes as private
+  //provide public get and set methods to access and update the value of a private variable
+   private String name;
+
+   // Getter
+   public String getName() {
+     return name;
+   }
+   // Setter
+   public void setName(String newName) {
+     this.name = newName;
+   }
+}
