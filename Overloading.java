fe5dbf1663b2c36ac63894550fc18dcdec6a7a5f
Carolina Martins <carolina.martins3@pucpr.edu.br>
2023-05-16 10:40:12 -0300

    Update Overloading.java

--- Overloading.java
+++ Overloading.java
@@ -8,7 +8,7 @@ public class Overloading {
   }
   
   public static void main(String[] args) {
-    int number1 = conta(12, 8);
+    int number1 = conta(12, 10);
     double number2 = conta(2.25, 9.30);
     System.out.println("Conta int: " + number1);
     System.out.println("Conta double: " + number2);
